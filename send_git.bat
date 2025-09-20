17e7dca91a20d1f341c7d0f948d16520efef13bb
Edson2010hmc <edson2010hmc@gmail.com>
2025-09-20 11:59:38 -0300

    20/09/2025 - 11:59:38,27

---
+++ send_git.bat
@@ -0,0 +1,7 @@
+git init
+git add .
+git commit -m "%date% - %time%"
+git config --global user.name "Edson2010hmc"
+git remote add origin https://github.com/Edson2010hmc/psweb_py.git
+git branch -M main
+git push -u origin main
